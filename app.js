7d65d533575a09944cb2f0f7dceabc3254b6dab0
balaji-sivakumar <31851611+balaji-sivakumar@users.noreply.github.com>
2017-11-12 09:24:02 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -45,7 +45,10 @@ bot.dialog('Greeting', function (session, args, next) {
 		var userInfo = {
 			orderInfo : orderInfo,
 			email : '',
-			phoneNumber : ''
+			phoneNumber : '',
+			trackingNumber: '',
+			totalItemCount: '',
+			estimatedArrival: ''
 		};
 		session.userData.userInfo = userInfo;
 	}
@@ -87,8 +90,7 @@ bot.dialog('Order Status', function (session, args, next) {
 			console.log('Return value..'+status);
 			if(status){
 				session.send('We have sucessfully verified your details');
-				var status = sendOrderStatus(session,args);
-				session.send(status);
+				sendOrderStatus(session,args,jsonRes);
 			}else{
 				session.send('Sorry your details dont match, try again');
 			}
@@ -110,7 +112,7 @@ invokeATGService = function (session, args, jsonRes){
 	var orderJSON = "";
     console.log("statusCode: ", res.statusCode);
 		res.on('data', function(d) {
-			console.info('GET result:\n');
+			console.info('GET result:\n'+d);
 			orderJSON = JSON.parse(d.toString());
 		});
 		res.on('end', function() {
@@ -122,48 +124,178 @@ invokeATGService = function (session, args, jsonRes){
 	}).end();
 }
 
-sendOrderStatus = function (session, args){
-	var msg = new builder.Message(session).addAttachment({
+sendOrderStatus = function (session, args, jsonRes){
+	var email = jsonRes.paymentResult[0].billingAddress.email;
+	if(email == null){
+		email = "";
+	}
+	var phone = jsonRes.paymentResult[0].billingAddress.phoneNumber;
+	if(phone == null){
+		phone = "";
+	}
+	var trackingNum = jsonRes.shippingResult[0].trackingNumber;
+	var trackingURL = "";
+	if(trackingNum != undefined && trackingNum!= null){
+		trackingURL =  "http://www.fedex.com?trackingNum="+trackingNum;
+	}else{
+		trackingURL =  "http://www.fedex.com?trackingNum="+789876;
+	}
+	var totalItemCount = (jsonRes.result.totalCommerceItemCount).toString();
+	var name = jsonRes.paymentResult[0].billingAddress.firstName + 
+		" " + jsonRes.paymentResult[0].billingAddress.lastName;
+	var orderId = jsonRes.result.id;
+	var items = jsonRes.result.commerceItems;
+	var paymentInfo = jsonRes.paymentResult[0].creditCardType +
+		" ************" + jsonRes.paymentResult[0].creditCardNumber;
+	var tax = jsonRes.result.priceInfo.tax;
+	var total = jsonRes.result.priceInfo.total;
+	var shipState = jsonRes.shippingResult[0].state;
+	var total = (jsonRes.result.priceInfo.total).toString();
+	total = total.substring(0, total.indexOf('.')+3);
+	var today = new Date();
+	var msg;
+	if(shipState != undefined && shipState == 104){
+		today.setDate(today.getDate() + 1);
+		var stringDate = today.toDateString();
+		msg = new builder.Message(session).addAttachment({
 		contentType: "application/vnd.microsoft.card.adaptive",
 		content: {
-			type: "AdaptiveCard",
+			"$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+			"type": "AdaptiveCard",
 			"version": "1.0",
-			body: [
-				{
-                        "type": "TextBlock",
-                        "text": "Your Order Details",
-                        "size": "large",
-                        "weight": "bolder"
+			"body": [
+					{
+						"type": "Container",
+						"items": [
+							{
+								"type": "TextBlock",
+								"text": "Your Order Status",
+								"weight": "bolder",
+								"size": "medium"
+							}
+						]
 					},
 					{
-                        "type": "TextBlock",
-                        "text": "Order Number#"+ session.userData.userInfo.orderInfo.orderId
+						"type": "Container",
+						"items": [
+							{
+								"type": "FactSet",
+								"separator": true,
+								"facts": [
+									{
+										"title": "Order Number",
+										"value": orderId
+									},
+									{
+										"title": "Full Name",
+										"value": name
+									},
+									{
+										"title": "Payment Type",
+										"value": paymentInfo
+									},
+									{
+										"title": "Order Total",
+										"value": total
+									},
+									{
+										"title": "Items",
+										"value": totalItemCount
+									},
+									{
+										"title": "Status",
+										"value": "Shipped, On your way!"
+									},
+									{
+										"title": "Tracking",
+										"value": trackingURL
+									},
+									{
+										"title": "Estimated Arrival",
+										"value": stringDate
+									},
+									{
+										"title": "Contact",
+										"value": email + "/" + phone
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		});		
+	}else{
+		today.setDate(today.getDate() + 3);
+		var stringDate = today.toDateString();
+		console.log(stringDate);
+		msg = new builder.Message(session).addAttachment({
+		contentType: "application/vnd.microsoft.card.adaptive",
+		content: {
+			"$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+			"type": "AdaptiveCard",
+			"version": "1.0",
+			"body": [
+					{
+						"type": "Container",
+						"items": [
+							{
+								"type": "TextBlock",
+								"text": "Your Order Status",
+								"weight": "bolder",
+								"size": "medium"
+							}
+						]
 					},
 					{
-                        "type": "TextBlock",
-                        "text": "Name: Balaji Sivakumar"
-					},
-					{
-                        "type": "TextBlock",
-                        "text": "Email: "+ session.userData.userInfo.email
-                    },
-                    {
-                        "type": "TextBlock",
-                         "text": "Mobile: "+ session.userData.userInfo.phoneNumber
-                    },
-                    {
-                        "type": "TextBlock",
-                         "text": "Order Status: Shipped, On your way!"
-                    },
-                    {
-						"type": "Action.OpenUrl",
-						"title": "Track your order",
-                        "url": "http://www.fedex.com?trackingNum=09879"
-                    }
-			]
-		}
-	});
-	return msg;
+						"type": "Container",
+						"items": [
+							{
+								"type": "FactSet",
+								"separator": true,
+								"facts": [
+									{
+										"title": "Order Number",
+										"value": orderId
+									},
+									{
+										"title": "Full Name",
+										"value": name
+									},
+									{
+										"title": "Payment Type",
+										"value": paymentInfo
+									},
+									{
+										"title": "Order Total",
+										"value": total
+									},
+									{
+										"title": "Items",
+										"value": totalItemCount
+									},
+									{
+										"title": "Status",
+										"value": "Your Order is currently being processed!"
+									},
+									{
+										"title": "Estimated Arrival",
+										"value": stringDate
+									},
+									{
+										"title": "Contact",
+										"value": email + "/" + phone
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		});
+	}
+	
+	session.send(msg);
 }
 
 
